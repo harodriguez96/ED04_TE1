16c71553b189f9e036d41ba6a75e306ea40e85cc
haize <haize@Haizea>
2024-05-11 14:35:05 +0200

    Cuarto Commit

--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -10,14 +10,14 @@ public class Main {
         cuentaTrabajo = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
         saldoActual = cuentaTrabajo.getSaldo();
         System.out.println("El saldo actual es " + saldoActual);
-        operativaCuenta(cuentaTrabajo);
+        operativaCuenta(cuentaTrabajo,2300);
         
     }
     
-    public static void operativaCuenta(CCuenta cuentaTrabajo) {
+    public static void operativaCuenta(CCuenta cuentaTrabajo, double cantidad) {
     	try {
             System.out.println("Cargo en cuenta");
-            cuentaTrabajo.retirar(2300);
+            cuentaTrabajo.retirar(cantidad);
             System.out.println("El nuevo saldo es: " + cuentaTrabajo.getSaldo());
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
